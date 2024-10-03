db6175d42321b9fed95b12c10a7db7f0582ce64b
bchavs12 <chavesbruno699@gmail.com>
2024-10-02 23:08:53 -0300

    fix: set/clear new interval

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -42,12 +42,18 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -63,8 +69,12 @@ export function Home() {
 
     setCycles((state: any) => [...state, newCycle])
     setActiveCycleId(id);
+    setAmountSecondsPassed(0)
+
     reset();
   }
+
+
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondPassed : 0
@@ -74,6 +84,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch("task");
   const isSubmitDisable = !task;
